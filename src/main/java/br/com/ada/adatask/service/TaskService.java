f20db1ea58768ed634b52ec0b0cd5b5c7a39ad4f
rewenila <32466162+rewenila@users.noreply.github.com>
2024-01-27 17:12:36 -0300

    implemented methods to get task by id and filter task by type

--- src/main/java/br/com/ada/adatask/service/TaskService.java
+++ src/main/java/br/com/ada/adatask/service/TaskService.java
@@ -1,7 +1,11 @@
 package br.com.ada.adatask.service;
 
+import br.com.ada.adatask.domain.BaseTask;
 import br.com.ada.adatask.domain.Task;
 import br.com.ada.adatask.repository.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskService {
 
@@ -11,8 +15,49 @@ public class TaskService {
         this.repository = repository;
     }
 
-    public void saveNewTask(Task task) {
+    public void createTask(Task task) {
         System.out.println("Saving task in repository " + this.repository.getClass().getName());
-        repository.create(task);
+        repository.save(task);
     }
+
+    public void updateTask(BaseTask existingTask, BaseTask newTask) {
+        System.out.println("Editing task " + existingTask.getTitle());
+        repository.delete(existingTask);
+        repository.save(newTask);
+    }
+
+    public void deleteTask(Task task) {
+        System.out.println("Deleting task " + task.toString());
+        repository.delete(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return repository.getAll();
+    }
+
+    public Task getTaskById(Long id) {
+        List<Task> allTasks = repository.getAll();
+
+        for(Task task : allTasks) {
+            if (id.equals(task.getId())) {
+                return task;
+            }
+        }
+
+        return null;
+    }
+
+    public <T extends Task> List<T> filterByTaskType(Class<T> taskType) {
+        List<Task> allTasks = repository.getAll();
+        List<T> filteredTasks = new ArrayList<>();
+
+        for (Task task : allTasks) {
+            if (taskType.isInstance(task)) {
+                filteredTasks.add(taskType.cast(task));
+            }
+        }
+
+        return filteredTasks;
+    }
+
 }
